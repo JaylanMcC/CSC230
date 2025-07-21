76d3d935f77c4d95b279147f3bbaf2f7813e7e65
JaylanMcC <150658818+JaylanMcC@users.noreply.github.com>
2025-07-21 15:36:19 -0700

    Create secret.h

---
+++ secret.h
@@ -0,0 +1,4 @@
+#define SECRET_SSID "Scytale"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "Chaumurky26"	// replace MyPassword with your WiFi password
+#define FIREBASE_HOST "fir-28b8a-default-rtdb.firebaseio.com"
+#define FIREBASE_AUTH "AIzaSyCBMPUjqpT_H9ic4g-h_5GCQACQ2_3b3Lw"
